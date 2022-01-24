a904a1f3e0466940c665ac7b6e6a58503c5d734d
Jiří Bětuňák <61905170+KinDR007@users.noreply.github.com>
2022-01-24 18:48:31 +0100

    up

--- components/victron/__init__.py
+++ components/victron/__init__.py
@@ -1,17 +1,29 @@
-import esphome.codegen as cg import esphome.config_validation as cv from esphome.components import uart from esphome.const import CONF_ID
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart
+from esphome.const import CONF_ID
 
-    AUTO_LOAD =["sensor", "text_sensor"]
+AUTO_LOAD = ["sensor", "text_sensor"]
 
-                DEPENDENCIES =["uart"]
+DEPENDENCIES = ["uart"]
 
-                CODEOWNERS =["@KinDR007"]
+CODEOWNERS = ["@KinDR007"]
 
-                             MULTI_CONF = True
+MULTI_CONF = True
 
-                             victron_ns = cg.esphome_ns.namespace("victron") VictronComponent = victron_ns.class_("VictronComponent", uart.UARTDevice, cg.Component)
+victron_ns = cg.esphome_ns.namespace("victron")
+VictronComponent = victron_ns.class_("VictronComponent", uart.UARTDevice, cg.Component)
 
-                                                                                                                                                           CONF_VICTRON_ID = "victron_id"
+CONF_VICTRON_ID = "victron_id"
 
-                                                                                                                  CONFIG_SCHEMA = uart.UART_DEVICE_SCHEMA.extend({cv.GenerateID() : cv.declare_id(VictronComponent), })
+CONFIG_SCHEMA = uart.UART_DEVICE_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(VictronComponent),
+    }
+)
 
-                                                                                                                                                                      def to_code(config) : var = cg.new_Pvariable(config[CONF_ID]) yield cg.register_component(var, config) yield uart.register_uart_device(var, config)
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    yield uart.register_uart_device(var, config)
