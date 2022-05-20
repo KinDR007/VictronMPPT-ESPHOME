547b1fa35565be633364853d61c4c1e60b8e535b
Sebastian Muszynski <s.muszynski@constructiva.de>
2022-05-20 17:16:44 +0200

    Introduce throttle parameter
    Fixes: #46

--- components/victron/__init__.py
+++ components/victron/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_THROTTLE
 
 AUTO_LOAD = ["binary_sensor", "sensor", "text_sensor"]
 
@@ -19,6 +19,7 @@ CONF_VICTRON_ID = "victron_id"
 CONFIG_SCHEMA = uart.UART_DEVICE_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(VictronComponent),
+        cv.Optional(CONF_THROTTLE, default="1s"): cv.positive_time_period_milliseconds,
     }
 )
 
@@ -27,3 +28,5 @@ def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield uart.register_uart_device(var, config)
+
+    cg.add(var.set_throttle(config[CONF_THROTTLE]))
