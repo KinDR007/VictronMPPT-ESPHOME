28827157aa422494a1e97119f045c86b81406713
Orestes Sanchez Benavente <orestes@estotienearreglo.es>
2025-09-17 13:58:24 +0200

    Use string literals for logging in Victron component

--- components/victron/victron.cpp
+++ components/victron/victron.cpp
@@ -29,7 +29,7 @@ static const char *const OFF_REASONS[OFF_REASONS_SIZE] = {
 };
 
 void VictronComponent::dump_config() {  // NOLINT(google-readability-function-size,readability-function-size)
-  ESP_LOGCONFIG(TAG, LOG_STR_LITERAL("Victron:"));
+  ESP_LOGCONFIG(TAG, "Victron:");
   LOG_BINARY_SENSOR("  ", LOG_STR_LITERAL("Load state"), load_state_binary_sensor_);
   LOG_BINARY_SENSOR("  ", "Relay state", relay_state_binary_sensor_);
   LOG_SENSOR("  ", LOG_STR_LITERAL("Max Power Yesterday"), max_power_yesterday_sensor_);
