c254032dc445e98ac491494f6407e368f9611ae3
Sebastian Muszynski <basti@linkt.de>
2026-05-21 23:48:00 +0200

    Add component schema tests and pytest CI job (#242)

--- components/victron/binary_sensor.py
+++ components/victron/binary_sensor.py
@@ -11,22 +11,22 @@ CODEOWNERS = ["@KinDR007"]
 CONF_LOAD_STATE = "load_state"
 CONF_RELAY_STATE = "relay_state"
 
-BINARY_SENSORS = [
-    CONF_LOAD_STATE,
-    CONF_RELAY_STATE,
-]
+BINARY_SENSOR_DEFS = {
+    CONF_LOAD_STATE: {},
+    CONF_RELAY_STATE: {},
+}
 
 CONFIG_SCHEMA = VICTRON_COMPONENT_SCHEMA.extend(
     {
-        cv.Optional(CONF_LOAD_STATE): binary_sensor.binary_sensor_schema(),
-        cv.Optional(CONF_RELAY_STATE): binary_sensor.binary_sensor_schema(),
+        cv.Optional(key): binary_sensor.binary_sensor_schema(**kwargs)
+        for key, kwargs in BINARY_SENSOR_DEFS.items()
     }
 )
 
 
 def to_code(config):
     hub = yield cg.get_variable(config[CONF_VICTRON_ID])
-    for key in BINARY_SENSORS:
+    for key in BINARY_SENSOR_DEFS:
         if key in config:
             conf = config[key]
             sens = yield binary_sensor.new_binary_sensor(conf)
---
+++ tests/test_component_schemas.py
@@ -0,0 +1,94 @@
+"""Component schema tests."""
+
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+
+
+class TestSensorDefs:
+    def test_sensor_defs_completeness(self):
+        from components.victron.sensor import SENSOR_DEFS
+
+        assert len(SENSOR_DEFS) == 54
+
+    def test_first_and_last_sensor(self):
+        from components.victron.sensor import (
+            CONF_BATTERY_VOLTAGE,
+            CONF_DC_INPUT_POWER,
+            SENSOR_DEFS,
+        )
+
+        keys = list(SENSOR_DEFS)
+        assert keys[0] == CONF_BATTERY_VOLTAGE
+        assert keys[-1] == CONF_DC_INPUT_POWER
+
+    def test_known_sensors(self):
+        from components.victron.sensor import (
+            CONF_BATTERY_CURRENT,
+            CONF_BATTERY_VOLTAGE,
+            CONF_DC_INPUT_POWER,
+            CONF_DC_INPUT_VOLTAGE,
+            CONF_PANEL_VOLTAGE,
+            CONF_STATE_OF_CHARGE,
+            CONF_YIELD_TOTAL,
+            SENSOR_DEFS,
+        )
+
+        assert CONF_BATTERY_VOLTAGE in SENSOR_DEFS
+        assert CONF_PANEL_VOLTAGE in SENSOR_DEFS
+        assert CONF_BATTERY_CURRENT in SENSOR_DEFS
+        assert CONF_YIELD_TOTAL in SENSOR_DEFS
+        assert CONF_STATE_OF_CHARGE in SENSOR_DEFS
+        assert CONF_DC_INPUT_VOLTAGE in SENSOR_DEFS
+        assert CONF_DC_INPUT_POWER in SENSOR_DEFS
+
+
+class TestTextSensorConstants:
+    def test_text_sensor_count(self):
+        from components.victron.text_sensor import TEXT_SENSORS
+
+        assert len(TEXT_SENSORS) == 15
+
+    def test_first_and_last_text_sensor(self):
+        from components.victron.text_sensor import (
+            CONF_CHARGING_MODE,
+            CONF_OFF_REASON,
+            TEXT_SENSORS,
+        )
+
+        assert TEXT_SENSORS[0] == CONF_CHARGING_MODE
+        assert TEXT_SENSORS[-1] == CONF_OFF_REASON
+
+    def test_known_text_sensors(self):
+        from components.victron.text_sensor import (
+            CONF_CHARGING_MODE,
+            CONF_ERROR,
+            CONF_FIRMWARE_VERSION,
+            CONF_OFF_REASON,
+            CONF_TRACKING_MODE,
+            TEXT_SENSORS,
+        )
+
+        assert CONF_CHARGING_MODE in TEXT_SENSORS
+        assert CONF_ERROR in TEXT_SENSORS
+        assert CONF_TRACKING_MODE in TEXT_SENSORS
+        assert CONF_FIRMWARE_VERSION in TEXT_SENSORS
+        assert CONF_OFF_REASON in TEXT_SENSORS
+
+
+class TestBinarySensorDefs:
+    def test_binary_sensor_defs_completeness(self):
+        from components.victron.binary_sensor import BINARY_SENSOR_DEFS
+
+        assert len(BINARY_SENSOR_DEFS) == 2
+
+    def test_known_binary_sensors(self):
+        from components.victron.binary_sensor import (
+            BINARY_SENSOR_DEFS,
+            CONF_LOAD_STATE,
+            CONF_RELAY_STATE,
+        )
+
+        assert CONF_LOAD_STATE in BINARY_SENSOR_DEFS
+        assert CONF_RELAY_STATE in BINARY_SENSOR_DEFS
