1b30efabc66f7bdea3392e04a6e0ec40b19badda
Sebastian Muszynski <basti@linkt.de>
2026-05-21 22:49:40 +0200

    Explicitly initialize pointer members to nullptr (#236)

--- components/victron/victron.h
+++ components/victron/victron.h
@@ -223,8 +223,8 @@ class VictronComponent : public uart::UARTDevice, public Component {
   void publish_state_(text_sensor::TextSensor *text_sensor, const std::string &state);
   void publish_state_once_(text_sensor::TextSensor *text_sensor, const std::string &state);
 
-  binary_sensor::BinarySensor *load_state_binary_sensor_;
-  binary_sensor::BinarySensor *relay_state_binary_sensor_;
+  binary_sensor::BinarySensor *load_state_binary_sensor_{nullptr};
+  binary_sensor::BinarySensor *relay_state_binary_sensor_{nullptr};
 
   sensor::Sensor *max_power_yesterday_sensor_{nullptr};
   sensor::Sensor *max_power_today_sensor_{nullptr};
