48f0950c48bf5f5afa27ab563277dc714cb4bfa3
Sebastian Muszynski <basti@linkt.de>
2026-05-21 23:22:30 +0200

    Introduce SENSOR_DEFS and BINARY_SENSOR_DEFS, expand schema tests

--- tests/test_component_schemas.py
+++ tests/test_component_schemas.py
@@ -1,23 +1,42 @@
 """Component schema tests."""
 
 
-class TestSensorConstants:
-    def test_sensor_count(self):
-        from components.victron.sensor import SENSORS
+class TestSensorDefs:
+    def test_sensor_defs_completeness(self):
+        from components.victron.sensor import SENSOR_DEFS
 
-        assert len(SENSORS) == 54
+        assert len(SENSOR_DEFS) == 54
+
+    def test_first_and_last_sensor(self):
+        from components.victron.sensor import (
+            CONF_BATTERY_VOLTAGE,
+            CONF_DC_INPUT_POWER,
+            SENSOR_DEFS,
+        )
+
+        keys = list(SENSOR_DEFS)
+        assert keys[0] == CONF_BATTERY_VOLTAGE
+        assert keys[-1] == CONF_DC_INPUT_POWER
 
     def test_known_sensors(self):
         from components.victron.sensor import (
+            CONF_BATTERY_CURRENT,
             CONF_BATTERY_VOLTAGE,
+            CONF_DC_INPUT_POWER,
+            CONF_DC_INPUT_VOLTAGE,
             CONF_PANEL_VOLTAGE,
             CONF_STATE_OF_CHARGE,
-            SENSORS,
+            CONF_YIELD_TOTAL,
+            SENSOR_DEFS,
         )
 
-        assert CONF_BATTERY_VOLTAGE in SENSORS
-        assert CONF_PANEL_VOLTAGE in SENSORS
-        assert CONF_STATE_OF_CHARGE in SENSORS
+        assert CONF_BATTERY_VOLTAGE in SENSOR_DEFS
+        assert CONF_PANEL_VOLTAGE in SENSOR_DEFS
+        assert CONF_BATTERY_CURRENT in SENSOR_DEFS
+        assert CONF_YIELD_TOTAL in SENSOR_DEFS
+        assert CONF_STATE_OF_CHARGE in SENSOR_DEFS
+        assert CONF_DC_INPUT_VOLTAGE in SENSOR_DEFS
+        assert CONF_DC_INPUT_POWER in SENSOR_DEFS
 
 
 class TestTextSensorConstants:
@@ -26,29 +45,45 @@ class TestTextSensorConstants:
 
         assert len(TEXT_SENSORS) == 15
 
+    def test_first_and_last_text_sensor(self):
+        from components.victron.text_sensor import (
+            CONF_CHARGING_MODE,
+            CONF_OFF_REASON,
+            TEXT_SENSORS,
+        )
+
+        assert TEXT_SENSORS[0] == CONF_CHARGING_MODE
+        assert TEXT_SENSORS[-1] == CONF_OFF_REASON
+
     def test_known_text_sensors(self):
         from components.victron.text_sensor import (
             CONF_CHARGING_MODE,
             CONF_ERROR,
+            CONF_FIRMWARE_VERSION,
+            CONF_OFF_REASON,
+            CONF_TRACKING_MODE,
             TEXT_SENSORS,
         )
 
         assert CONF_CHARGING_MODE in TEXT_SENSORS
         assert CONF_ERROR in TEXT_SENSORS
+        assert CONF_TRACKING_MODE in TEXT_SENSORS
+        assert CONF_FIRMWARE_VERSION in TEXT_SENSORS
+        assert CONF_OFF_REASON in TEXT_SENSORS
 
 
-class TestBinarySensorConstants:
-    def test_binary_sensor_count(self):
-        from components.victron.binary_sensor import BINARY_SENSORS
+class TestBinarySensorDefs:
+    def test_binary_sensor_defs_completeness(self):
+        from components.victron.binary_sensor import BINARY_SENSOR_DEFS
 
-        assert len(BINARY_SENSORS) == 2
+        assert len(BINARY_SENSOR_DEFS) == 2
 
     def test_known_binary_sensors(self):
         from components.victron.binary_sensor import (
-            BINARY_SENSORS,
+            BINARY_SENSOR_DEFS,
             CONF_LOAD_STATE,
             CONF_RELAY_STATE,
         )
 
-        assert CONF_LOAD_STATE in BINARY_SENSORS
-        assert CONF_RELAY_STATE in BINARY_SENSORS
+        assert CONF_LOAD_STATE in BINARY_SENSOR_DEFS
+        assert CONF_RELAY_STATE in BINARY_SENSOR_DEFS
